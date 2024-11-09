0c7fb165faa0e867fd94e6c4ecc03cd3fbc639bf
Aarontobar <80783064+Aarontobar@users.noreply.github.com>
2024-11-09 17:09:10 -0300

    creo queya

--- Fase 2/Evidencias Proyecto/Evidencias de sistema/MeseroApp/src/config.js
+++
@@ -1,8 +0,0 @@
-// src/config.js
-const config = {
-    serverIp: '192.168.100.6', // Cambia esto a la IP de tu servidor
-    serverPort: '80', // Cambia esto si tu servidor usa un puerto diferente
-    basePath: '/capstone/php/' // Ruta base para tus scripts PHP
-  };
-  
-  export default config;
--- Fase 2/Evidencias Proyecto/Evidencias de sistema/MeseroApp/src/utils/api.js
+++
@@ -1,6 +0,0 @@
-// src/utils/api.js
-import config from '../config';
-
-export const getApiUrl = (endpoint) => {
-  return `http://${config.serverIp}:${config.serverPort}${config.basePath}${endpoint}`;
-};
